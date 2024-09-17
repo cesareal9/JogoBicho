8347d19f612851d257145d47276399852be79929
cesareal9 <caugustorama@gmail.com>
2024-09-16 21:22:54 -0300

    adicionado conn.js

---
+++ src/conn.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const mongoDB = "mongodb://localhost:27017";
+const database = "JogoBicho";
+
+const main = async () => {
+    try {
+        await mongoose.connect(mongoDB + database);
+        console.log("Conectado com sucesso");
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+main();
+
+export default mongoose;
