c14734528d0518353734559057d8a77599e402ff
cesareal9 <caugustorama@gmail.com>
2024-09-18 19:55:38 -0300

    adicionado app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,12 @@
 import "dotenv/config";
 import e from "express";
 
+const Aposta = require("./routes/Aposta.js");
+const JogoBicho = require("./routes/JogoBicho.js");
+
+app.use("/Aposta", Apostas);
+app.use("/JogoBicho", JogoBichos);
+
 const app = e();
 app.listen(process.env.API_PORT, () => {
     console.log(`Server Running in port ${process.env.API_PORT}`);
